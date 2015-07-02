68b10838ad1907ed53fa73ceb2f8fc3fc6326478
muhsinking <muhsinking@gmail.com>
2015-07-02 04:25:49 +0100

    complete contact form using mandrill, hide apikey from server.js

--- server.js
+++ server.js
@@ -2,30 +2,59 @@ var express = require('express');
 var nodemailer = require("nodemailer");
 var app = express();
 
-var smtpTransport = nodemailer.createTransport("SMTP",{
-	service: "Gmail",
-	auth: {
-	user: "yourID@gmail.com"
-	pass: "your gmail password"
-	}
-});
+var fs = require('fs');
+var mandrillApi = JSON.parse(fs.readFileSync('private/apikey.json','utf8'));
 
-app.get('/',function(req,res){
-	res.sendfile('index.html');
-});
-app.get('/send',function(req,res){
-	var mailOptions = {
-	to : req.query.to
-	subject : req.query.subject,
-	text : req.query.text
+app.use(express.static(__dirname + '/public'));
+
+var mandrillTransport = require('nodemailer-mandrill-transport');
+
+var transporter = nodemailer.createTransport(mandrillTransport({
+	auth: {
+		apiKey: mandrillApi.key
 	}
-	console.log(mailOptions);
-	smtpTransport.sendMail(mailOptions, function(error, response){
+}));
+
+/*
+transporter.sendMail(
+	{
+	from: 'Mo King <muhsinking@gmail.com>',
+	to: 'muhsinking@gmail.com',
+	subject: 'hello world!',
+	text: 'Mandrilled'
+	},
+	function(error,info){
 		if (error){
 			console.log(error);
-			res.end("error");
+		}
+		else{
+			console.log("Message sent");
+		}
+});
+*/
+
+
+app.get('/', function(req,res){
+	res.sendfile('index.html');
+});
+
+app.get('/send', function(req,res){
+	
+	var mailOptions = {
+		from: req.query.from,
+		to : 'muhsinking@gmail.com',
+		subject : req.query.subject,
+		text : req.query.text
+	}
+	
+	console.log(mailOptions);
+	
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.end("errorrrrr");
 		}else{
-			console.log("Message sent: " + response.message);
+			console.log("Message sent!");
 			res.end("sent");
 		}
 	});
