973f96df24bc9a31237710a1b37088d819c8e8f1
Mo King <muhsinking@gmail.com>
2015-07-15 16:55:15 -0500

    create skeleton html for skills rows

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var transporter = nodemailer.createTransport(mandrillTransport({
 }));
 
 app.get('/', function(req,res){
-	res.sendFile('index.html');
+	res.sendfile('index.html');
 });
 
 app.get('/send', function(req,res){
